6ee4771b4c68057de8bbb47916bd5878e87bad9c
arittrolite-ux <arittrolite@gmail.com>
2026-06-06 22:02:11 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+function cmToMeter(){
+    let cm = document.getElementById("cm").value;
+    document.getElementById("cmResult").innerHTML =
+    (cm/100) + " m";
+}
 
+function kgToGram(){
+    let kg = document.getElementById("kg").value;
+    document.getElementById("kgResult").innerHTML =
+    (kg*1000) + " g";
+}
+
+function cToF(){
+    let c = document.getElementById("celsius").value;
+    document.getElementById("tempResult").innerHTML =
+    ((c*9/5)+32) + " °F";
+}
+
+function mpaToPsi(){
+    let m = document.getElementById("mpa").value;
+    document.getElementById("mpaResult").innerHTML =
+    (m*145.038).toFixed(2) + " psi";
+}
